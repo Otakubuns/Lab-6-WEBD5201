dedc840e5b02dfc934227345418574351259777b
Alexus <77337386+Otakubuns@users.noreply.github.com>
2023-12-12 15:11:00 -0500

    initial commit

---
+++ src/main/java/com/example/servingwebcontent100727793/ServingWebContent100727793Application.java
@@ -0,0 +1,13 @@
+package com.example.servingwebcontent100727793;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContent100727793Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServingWebContent100727793Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/servingwebcontent100727793/ServingWebContent100727793ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servingwebcontent100727793;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServingWebContent100727793ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
